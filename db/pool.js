5172a2a14be755442b656b29ec86d60e5e96e141
Samado Xen0w <benazouz.souheilabdessamed@gmail.com>
2024-12-31 00:53:25 +0100

    5eme commit

--- db/pool.js
+++ db/pool.js
@@ -7,7 +7,7 @@ const { Pool } = pkg;
 export const pool = new Pool({
     host:
         process.env.DB_HOST ||
-        "postgresql://top_users_uk9p_user:vbOu2bB8zqPNTZn0Gt6rNjf5W9Zc9Pac@dpg-ctpicut2ng1s73ds8c5g-a.frankfurt-postgres.render.com/top_users_uk9p",
+        "postgresql://top_users_uk9p_user:vbOu2bB8zqPNTZn0Gt6rNjf5W9Zc9Pac@dpg-ctpicut2ng1s73ds8c5g-a/top_users_uk9p",
     user: process.env.DB_USER || "postgres",
     database: process.env.DB_NAME || "top_users",
     password: process.env.DB_PASSWORD || "0077",
